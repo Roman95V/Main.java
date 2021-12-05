c5b1003f227be2d0a558efd0a158d5efeefa9899
voloshynromanmukol@gmail.com <voloshynromanmukol@gmail.com>
2021-12-05 21:49:50 +0200

    update

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,26 +4,26 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        int num_1 = 4;
+        int num_1 = -4;
         int num_2 = 3;
         int num_3 = 3;
         int num_4 = 9;
 
         if (num_1 <= num_2 && num_1 <= num_3 && num_1 <= num_4)
         {
-            System.out.println("Min first number - " + num_1);
+            System.out.println("Min number: " + num_1);
         }
-        if (num_2 <= num_1 && num_2 <= num_3 && num_2 <= num_4)
+        else if (num_2 <= num_1 && num_2 <= num_3 && num_2 <= num_4)
         {
-            System.out.println("Min second number - " + num_2);
+            System.out.println("Min number: " + num_2);
         }
-        if (num_3 <= num_1 && num_3 <= num_2 && num_3 <= num_4)
+        else if (num_3 <= num_1 && num_3 <= num_2 && num_3 <= num_4)
         {
-            System.out.println("Min third number - " + num_3);
+            System.out.println("Min number: " + num_3);
         }
-        if (num_4 <= num_1 && num_4 <= num_2 && num_4 <= num_3)
+        else if (num_4 <= num_1 && num_4 <= num_2 && num_4 <= num_3)
         {
-            System.out.println("Min fourth number - " + num_4);
+            System.out.println("Min number: " + num_4);
         }
     }
 }
